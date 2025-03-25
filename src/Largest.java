e8a31eb2b37f53daf1bce748fec572024b47d3ba
sudhanshu20204 <sidshekhar2024@gmail.com>
2025-03-25 16:46:47 +0530

    chages

---
+++ src/Largest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        int max = a;
+        if(max<b) max=b;
+        if(max<c) max=c;
+        System.out.println("Max is :"+max);
+    }
+}
---
+++ src/Loop.java
@@ -0,0 +1,11 @@
+public class Loop
+{
+    public static void  main(String[] args){
+        System.out.println("sid");
+        for( int num =1;num<=5;num++){
+            System.out.println(num);
+
+        }
+
+    }
+}
